f3b562e054624b2ec37edbb92dd21cecb092d1bf
sonicboomslang <philhargrove@hotmail.com>
2025-04-08 18:28:29 -0400

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,40 @@
+import './App.css';
+import { useState } from 'react'
+import { puppyList } from './data.js'
 
-      <div>
-       
-      </div>
-      
+function App() {
+  const [puppies, setPuppies] = useState(puppyList)
+  const [featPupId, setFeatPupId] = useState(null)
+
+  const featuredPup = puppies.find((pup) => pup.id === featPupId)
+  console.log("featuredPup: ", featuredPup)
+
+  return (
+    <div className="App">
+      {puppies.map((puppy) => {
+        return (
+          <p
+            key={puppy.id}
+            onClick={() => {
+              setFeatPupId(puppy.id)
+            }}
+          >
+            {puppy.name}
+          </p>
+        )
+      })}
+
+      {featPupId && featuredPup && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default App
